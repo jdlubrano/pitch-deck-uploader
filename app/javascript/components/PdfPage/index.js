31b81926e98e86833ad20f8b6eaaf1b32950c693
Joel Lubrano <joel.lubrano@stitchfix.com>
2021-06-03 10:47:37 -0400

    Extract PdfPage component to preview uploaded pitch decks

---
+++ app/javascript/components/PdfPage/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+
+const PdfPage = ({ pdfPage }) => {
+  const canvasRef = useRef(null);
+  const viewport = pdfPage.getViewport({scale: 0.4});
+
+  async function renderPdfPage() {
+    const canvas = canvasRef.current;
+    const canvasContext = canvas.getContext('2d');
+
+    const renderContext = {
+    canvasContext,
+    viewport
+    };
+
+    await pdfPage.render(renderContext).promise;
+  }
+
+  useEffect(() => { renderPdfPage() }, [canvasRef]);
+
+  return (
+    <canvas ref={canvasRef} height={viewport.height} width={viewport.width} />
+  );
+};
+
+export default PdfPage;
--- app/javascript/components/PitchDeckForm/index.js
+++ app/javascript/components/PitchDeckForm/index.js
@@ -2,45 +2,32 @@ import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
+import PdfPage from "../PdfPage";
+
 const pdfjs = require("pdfjs-dist/webpack");
 
 const PitchDeckForm = ({ pitchDeck }) => {
-  const [convertingPdf, setConvertingPdf] = useState(false);
   const [name, setName] = useState(pitchDeck.name);
-  const [pitchDeckSlides, setPitchDeckSlides] = useState([]);
+  const [pdf, setPdf] = useState(null);
+  const [previewPage, setPreviewPage] = useState(null);
   const fileInputRef = useRef(null);
 
   const handleFileChosen = async (event) => {
+    setPdf(null);
+    setPreviewPage(null);
+
     const file = fileInputRef.current.files[0];
 
     if (!file) {
       return;
     }
 
-    const slides = [];
     const buffer = await file.arrayBuffer();
     const pdf = await pdfjs.getDocument(buffer).promise;
+    const page = await pdf.getPage(1);
 
-    for (let i = 1; i <= pdf.numPages; i++) {
-      const page = await pdf.getPage(i);
-      const viewport = page.getViewport({scale: 0.5});
-      const canvas = document.createElement("canvas");
-      const canvasContext = canvas.getContext('2d');
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-
-      const renderContext = {
-        canvasContext,
-        viewport
-      };
-
-      await page.render(renderContext).promise;
-      document.body.appendChild(canvas);
-      slides.push(canvas.toDataURL());
-    }
-
-    console.log(slides[0]);
-    setPitchDeckSlides(slides);
+    setPdf(pdf);
+    setPreviewPage(page);
   };
 
   const submitPitchDeck = event => {
@@ -48,40 +35,58 @@ const PitchDeckForm = ({ pitchDeck }) => {
     console.log("Submit form", name, fileInputRef.current.value);
   };
 
+  const resetState = () => {
+    setName("");
+    setPdf(null);
+    setPreviewPage(null);
+  };
+
   return (
-    <form className="row" onSubmit={submitPitchDeck}>
-      <p className="col-12 mb-2">
-        Upload a New Pitch Deck
-      </p>
-      <div className="col-12 mb-4">
-        <small>
-          <Link to="/pitch_decks">Back to pitch decks</Link>
-        </small>
-      </div>
-      <div className="col-12 mb-3">
-        <label htmlFor="pitchDeckName" className="form-label">Name</label>
-        <input
-          type="text"
-          className="form-control"
-          id="pitchDeckName"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          required />
-      </div>
-      <div className="col-12 mb-3">
-        <input
-          ref={fileInputRef}
-          type="file"
-          className="form-control"
-          aria-label="pitch deck file"
-          onChange={handleFileChosen}
-          required />
-      </div>
-      <div>
-        <button className="btn btn-primary me-3" type="submit">Submit</button>
-        <button className="btn btn-light" type="reset">Clear</button>
-      </div>
-    </form>
+    <>
+      <form className="row mb-3" onReset={resetState} onSubmit={submitPitchDeck}>
+        <p className="col-12 mb-2">
+          Upload a New Pitch Deck
+        </p>
+        <div className="col-12 mb-4">
+          <small>
+            <Link to="/pitch_decks">Back to pitch decks</Link>
+          </small>
+        </div>
+        <div className="col-12 mb-3">
+          <label htmlFor="pitchDeckName" className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="pitchDeckName"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required />
+        </div>
+        <div className="col-12 mb-3">
+          <input
+            ref={fileInputRef}
+            type="file"
+            className="form-control"
+            aria-label="pitch deck file"
+            onChange={handleFileChosen}
+            required />
+        </div>
+        <div>
+          <button className="btn btn-primary me-3" type="submit">Submit</button>
+          <button className="btn btn-light" type="reset">Clear</button>
+        </div>
+      </form>
+      {pdf && previewPage && (
+        <div className="row">
+          <div className="col-12">
+            Showing page 1 of {pdf.numPages}
+          </div>
+          <div className="col-12">
+            <PdfPage pdfPage={previewPage} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
