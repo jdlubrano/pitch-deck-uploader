7078f6f177f9214a428a80e150a4df6de51392c8
Joel Lubrano <joel.lubrano@stitchfix.com>
2021-06-04 20:26:07 -0400

    Test DismissibleAlert component

--- app/javascript/components/DismissibleAlert/index.js
+++ app/javascript/components/DismissibleAlert/index.js
@@ -5,12 +5,12 @@ import Alert from 'bootstrap/js/dist/alert';
 const DismissibleAlert = ({ children, status }) => (
   <div className={`alert alert-${status} alert-dismissible fade show`} role="alert">
     {children}
-    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" data-testid="test-close"></button>
   </div>
 );
 
 DismissibleAlert.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.node.isRequired,
   status: PropTypes.oneOf(["success", "danger"])
 };
 
